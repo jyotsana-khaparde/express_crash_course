3dd84b0e7e2f8753db15e4297946db25ca3f3033
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-08-13 07:37:20 +0530

    added middleware, html files

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const path = require('path')
+const members = require('./members')
+const logger = require('./middleware/logger')
 
+// create an express application
 const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// init middleware
+app.use(logger)
+
+// GETs all members
+app.get('/api/members', (req, res) => {
+    res.json(members)
 })
+
+// use method use to when we want to include middleware
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
     console.log('Server started on port: ', PORT);
 })
---
+++ members.js
@@ -0,0 +1,9 @@
+const members = [
+    {
+        id: 1,
+        name: 'jyotsana',
+        email: 'jyotsana@gmail.com'
+    }
+]
+
+module.exports = members
