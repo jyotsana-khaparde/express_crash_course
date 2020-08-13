ced25c77fe1779f13799972aa07040d43c41dc2d
Jyotsana Khaparde <jyotsanakhaparde0597@gmail.com>
2020-08-13 07:56:23 +0530

    created api for getting 1 id from array

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.get('/api/members', (req, res) => {
     res.json(members)
 })
 
+// GETs single members
+app.get('/api/members/:id', (req, res) => {
+    const fount = members.some(member => member.id === parseInt(req.params.id))
+    if (fount) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+       res.status(400).json({message: `No member with  the id of ${req.params.id}`}) 
+    }
+})
+
 // use method use to when we want to include middleware
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
--- members.js
+++ members.js
@@ -1,8 +1,18 @@
 const members = [
     {
         id: 1,
-        name: 'jyotsana',
-        email: 'jyotsana@gmail.com'
+        name: 'jyotsana_1',
+        email: 'jyotsana_1@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'jyotsana_2',
+        email: 'jyotsana_2@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'jyotsana_3',
+        email: 'jyotsana_3@gmail.com'
     }
 ]
 
